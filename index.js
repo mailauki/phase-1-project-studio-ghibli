d003c9daef9a2dcd544d32534b1b43c43393f72f
Julie Evans <juliemevans17@gmail.com>
2021-12-22 16:23:29 -0600

    Render list

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ document.addEventListener("DOMContentLoaded", () => {
 // Event Listeners ----------------------------------
 
 // Renderings ---------------------------------------
+function renderList(movie) {
+  const cardsContainer = document.querySelector("#cards-container")
+
+  const card = document.createElement("div")
+  card.id = movie.id
+
+  const img = document.createElement("img")
+  img.setAttribute("src", movie.image)
+  card.appendChild(img)
+  
+  const h3 = document.createElement("h3")
+  h3.innerText = movie.title
+  card.appendChild(h3)
+
+  const h4 = document.createElement("h4")
+  h4.innerText = `${movie.release_date} • ${movie.director}` // | or • ?
+  card.appendChild(h4)
+
+  const rating = document.createElement("div")
+  const p = document.createElement("p")
+  p.innerText = `☆ ${movie.rt_score}` // ★ or ☆ ?
+  rating.appendChild(p)
+  card.appendChild(rating)
+
+  console.log(card)
+  cardsContainer.appendChild(card)
+}
 
 // Fetch Requests -----------------------------------
 function getList() {
@@ -18,5 +45,8 @@ function getList() {
     }
   })
   .then(response => response.json())
-  .then(movieData => console.log(movieData))
+  .then(movieData => {
+    console.log(movieData)
+    movieData.forEach(movie => renderList(movie))
+  })
 }
